73568b7752d97409aaebd487980c804e90f28fa0
Jennifer <jenniferroques@gmail.com>
2022-01-28 16:10:07 -0600

    Completed

---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Links(gitHub, linkedIn) {
+    return (
+        <>
+        <h3>Links</h3>
+        <a href={gitHub}>https://github.com/liza</a>
+        <a href={linkedIn}>https://www.linkedin.com/in/liza/</a>
+        </>
+    );
+}
+
+
+export default Links;
